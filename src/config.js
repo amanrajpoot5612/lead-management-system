ff6fc85fb1dd479fef3cb7652a8812a24eb96dbb
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-12-30 15:34:34 +0530

    fix: remove commented-out API_BASE for cleaner code

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE = `${process.env.VITE_API_BASE_PROD}/api/v1`;
+const API_BASE = `${import.meta.env.VITE_API_BASE_PROD}/api/v1`;
 
 // const API_BASE = "http://localhost:4000/api/v1";
 
