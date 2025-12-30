4828aa66fdbaf189ab6a5515b7d6a938d93687d0
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-12-30 14:24:17 +0530

    fix: update API_BASE to use VITE environment variables

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const API_BASE = "http://localhost:4000/api/v1";
+const API_BASE = `${process.env.VITE_API_BASE_PROD}/api/v1`;
+
+// const API_BASE = "http://localhost:4000/api/v1";
 
 export { API_BASE };
